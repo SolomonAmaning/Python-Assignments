033edacb2a186fb23653b7d54e57deebe1a8380b
SolomonAmaning <110782836+SolomonAmaning@users.noreply.github.com>
2022-09-27 07:11:57 +0530

    Add files via upload

---
+++ reading_prog.py
@@ -0,0 +1,13 @@
+user=input("Enter your fullname: " )
+with open(r'program.txt','r') as f:
+    print(f.read())  # all contents
+    print(f.readline()) # each line
+
+# add 4,5
+# add 9,10
+# sub 10,5
+# mul 6,6
+
+# needs correction and explanation. Forgot the explanation
+# finding it difficult to syntactically connect the user to read file and the add 4,5 etc
+
